e3629c46bbe94fd5eea24bd4720e4129414c3834
Yassine Patel <patel.bcpst@gmail.com>
2015-09-21 01:16:10 +0200

    Depot Script Python

---
+++ DM/DM02/Scripts_Python/scriptExo2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 07:40:45 2015
+
+@author: Yas
+"""
+from __future__ import division
+from math import log,exp
+
+def f(x):
+    """fonction f de l'exercice 1 du DM"""
+    y = log(exp(x)/x -1)
+    return y
